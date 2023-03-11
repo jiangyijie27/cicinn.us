c675756fe14c57355200b1773c66e2979799e299
yijiejiang <yijiejiang@tencent.com>
2023-03-12 00:24:41 +0800

    chore: chores

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState, useRef, useEffect } from "react"
 import lottie from "lottie-web"
+import { useSpring, animated, to } from "@react-spring/web"
+import { useDrag } from "@use-gesture/react"
 // import rainbow from "./assets/rainbow.json"
 // import rocket from "./assets/rocket.json"
 // import sun from "./assets/sun.json"
 
 const colors = ["red", "blue", "green", "black", "white", "purple"]
 const footerColors = ["red", "blue", "yellow", "black", "white", "purple"]
+const carouselImages = [
+  "https://wxa.wxs.qq.com/wxad-design/cpy/pic-0.jpg",
+  "https://wxa.wxs.qq.com/wxad-design/cpy/pic-1.jpg",
+  "https://wxa.wxs.qq.com/wxad-design/cpy/pic-2.jpg",
+  "https://wxa.wxs.qq.com/wxad-design/cpy/pic-3.jpg",
+  "https://wxa.wxs.qq.com/wxad-design/cpy/pic-4.jpg",
+]
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +22,97 @@ function App() {
   const intervalIndex = useRef(0)
   const lottie1Mounted = useRef(false)
   const lottie2Mounted = useRef(false)
+  const carouselIndex = useRef(0)
+  const [{ x: textX }] = useSpring(
+    () => ({
+      from: { x: 0 },
+      to: { x: 50 },
+      config: {
+        duration: 8000,
+      },
+      loop: true,
+    }),
+    []
+  )
+
+  const [{ carouselX }, api] = useSpring(() => ({
+    carouselX: 0,
+    // config: {
+    //   tension: 280,
+    //   friction: 60,
+    //   precision: 0.001,
+    //   mass: 1,
+    //   velocity: 0,
+    // },
+  }))
+
+  const bindDrag = useDrag(
+    ({ movement: [mx], active, event, swipe: [swipeX] }) => {
+      event.preventDefault()
+
+      if (swipeX !== 0) {
+        if (swipeX === 1 && carouselIndex.current === 0) {
+          api.start({
+            carouselX: 0,
+          })
+        } else if (
+          swipeX === -1 &&
+          carouselIndex.current === carouselImages.length - 1
+        ) {
+          api.start({
+            carouselX: carouselIndex.current * -280,
+          })
+        } else {
+          carouselIndex.current -= swipeX
+          api.start({
+            carouselX: carouselIndex.current * -280,
+          })
+        }
+      } else {
+        if (active) {
+          api.start({
+            carouselX:
+              -carouselIndex.current * 280 +
+              mx *
+                ((carouselIndex.current < 1 && mx > 0) ||
+                (carouselIndex.current > carouselImages.length - 2 && mx < 0)
+                  ? 0.4
+                  : 1),
+            immediate: true,
+          })
+        } else {
+          if (mx > 0) {
+            if (carouselIndex.current > 0) {
+              carouselIndex.current -= 1
+              api.start({
+                carouselX: carouselIndex.current * -280,
+              })
+            } else {
+              api.start({
+                carouselX: 0,
+              })
+            }
+          } else {
+            if (carouselIndex.current < carouselImages.length - 1) {
+              carouselIndex.current += 1
+              api.start({
+                carouselX: carouselIndex.current * -280,
+              })
+            } else {
+              api.start({
+                carouselX: carouselIndex.current * -280,
+              })
+            }
+          }
+        }
+      }
+    },
+    {
+      axis: "x",
+      pointer: { touch: true },
+      eventOptions: { passive: false },
+    }
+  )
 
   useEffect(() => {
     const letters = [
@@ -47,7 +147,7 @@ function App() {
           renderer: "svg",
           loop: true,
           autoplay: true,
-          path: 'https://raw.githubusercontent.com/jiangyijie27/cicinn.us/master/src/assets/rainbow.json',
+          path: "https://raw.githubusercontent.com/jiangyijie27/cicinn.us/master/src/assets/rainbow.json",
         })
       })
     }
@@ -60,7 +160,7 @@ function App() {
           renderer: "svg",
           loop: true,
           autoplay: true,
-          path: 'https://raw.githubusercontent.com/jiangyijie27/cicinn.us/master/src/assets/rocket.json',
+          path: "https://raw.githubusercontent.com/jiangyijie27/cicinn.us/master/src/assets/rocket.json",
         })
       })
     }
@@ -91,7 +191,12 @@ function App() {
         HAPPY
       </div> */}
       <div className="lines line-1">
-        <div className="lines-inner">
+        <animated.div
+          className="lines-inner"
+          style={{
+            transform: to([textX], (x) => `translate3d(-${x}%, 0, 0)`),
+          }}
+        >
           {Array.from(new Array(2), (_, i) => i).map((o) => {
             return (
               <div key={o}>
@@ -102,10 +207,15 @@ function App() {
               </div>
             )
           })}
-        </div>
+        </animated.div>
       </div>
       <div className="lines line-2">
-        <div className="lines-inner">
+        <animated.div
+          className="lines-inner"
+          style={{
+            transform: to([textX], (x) => `translate3d(-${50 - x}%, 0, 0)`),
+          }}
+        >
           {Array.from(new Array(2), (_, i) => i).map((o) => {
             return (
               <div key={o}>
@@ -116,10 +226,15 @@ function App() {
               </div>
             )
           })}
-        </div>
+        </animated.div>
       </div>
       <div className="lines line-3">
-        <div className="lines-inner">
+        <animated.div
+          className="lines-inner"
+          style={{
+            transform: to([textX], (x) => `translate3d(-${x}%, 0, 0)`),
+          }}
+        >
           {Array.from(new Array(2), (_, i) => i).map((o) => {
             return (
               <div key={o}>
@@ -130,10 +245,64 @@ function App() {
               </div>
             )
           })}
-        </div>
+        </animated.div>
       </div>
       <div className="carousel">
-        <div className="carousel-bg" />
+        <div className="carousel-bg">
+          <div>
+            <span className="carousel-letter">c</span>
+            <span className="carousel-letter">i</span>
+            <span className="carousel-letter">c</span>
+            <span className="carousel-letter">i</span>
+            <span className="carousel-letter">n</span>
+            <span className="carousel-letter">n</span>
+            <span className="carousel-letter">u</span>
+            <span className="carousel-letter">s</span>
+          </div>
+
+          <div>
+            <span className="carousel-text">c</span>
+            <span className="carousel-text">i</span>
+            <span className="carousel-text">c</span>
+            <span className="carousel-text">i</span>
+            <span className="carousel-text">n</span>
+            <span className="carousel-text">n</span>
+            <span className="carousel-text">u</span>
+            <span className="carousel-text">s</span>
+          </div>
+        </div>
+        <div className="carousel-inner-wrapper" {...bindDrag()}>
+          <animated.div
+            className="carousel-inner"
+            style={{
+              transform: to([carouselX], (x) => `translate3d(${x}px, 0, 0)`),
+            }}
+          >
+            {carouselImages.map((o, i) => (
+              <animated.div
+                key={o}
+                className={`carousel-item carousel-item-${i + 1}`}
+                style={{
+                  transformOrigin: to(
+                    [carouselX],
+                    (x) => `${(-(x + 280 * (i - 1)) / 280) * 50}% 100%`
+                  ),
+                  transform: to(
+                    [carouselX],
+                    (x) =>
+                      `translate3d(${
+                        8 + ((x + 280 * (i - 1)) / 280) * 8
+                      }%, 0, 0) rotate(${((x + 280 * i - 1) / 280) * 10}deg)`
+                  ),
+                }}
+              >
+                <img src={o} />
+              </animated.div>
+            ))}
+          </animated.div>
+        </div>
+        {/* <div className="carousel-arrow carousel-arrow-left" /> */}
+        {/* <div className="carousel-arrow carousel-arrow-right" /> */}
       </div>
       <div className="footer">
         <div>
