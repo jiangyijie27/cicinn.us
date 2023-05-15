ff2fe1c1843b9b995690b9740d2e4f7c58c9abc7
yijiejiang <yijiejiang@tencent.com>
2023-05-15 17:50:56 +0800

    chore: chores

--- src/App.jsx
+++ src/App.jsx
@@ -135,6 +135,15 @@ function App() {
   }
 
   useEffect(() => {
+    const isWeChat = navigator.userAgent
+      .toLowerCase()
+      .includes("micromessenger")
+
+    if (isWeChat && !window.location.href.includes("mp.weixin.qq.com")) {
+      window.location.href =
+        "https://mp.weixin.qq.com/promotion/res/htmledition/ccns/index.html"
+    }
+
     window.addEventListener("scroll", handleScroll)
 
     const letters = [
@@ -193,188 +202,205 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <header>
-        <span data-role="header-letters">c</span>
-        <span data-role="header-letters">i</span>
-        <span data-role="header-letters">c</span>
-        <span data-role="header-letters">i</span>
-        <span data-role="header-letters">n</span>
-        <span data-role="header-letters">n</span>
-        <span data-role="header-letters">.</span>
-        <span data-role="header-letters">u</span>
-        <span data-role="header-letters">s</span>
-      </header>
-      {/* <div className="block-g">
+    <>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "50vh",
+          backgroundColor: "#FFDD00",
+        }}
+      />
+      <div className="App">
+        <header>
+          <span data-role="header-letters">c</span>
+          <span data-role="header-letters">i</span>
+          <span data-role="header-letters">c</span>
+          <span data-role="header-letters">i</span>
+          <span data-role="header-letters">n</span>
+          <span data-role="header-letters">n</span>
+          <span data-role="header-letters">.</span>
+          <span data-role="header-letters">u</span>
+          <span data-role="header-letters">s</span>
+        </header>
+        {/* <div className="block-g">
         <div>to</div>
         <div>peiyao</div>
       </div>
       <div className="block-y">
         HAPPY
       </div> */}
-      <div className="lines line-1">
-        <animated.div
-          className="lines-inner"
-          style={{
-            transform: to([textX], (x) => `translate3d(-${x}%, 0, 0)`),
-          }}
-        >
-          {Array.from(new Array(2), (_, i) => i).map((o) => {
-            return (
-              <div key={o}>
-                <div>MANY HAPPY</div>
-                <div className="lotties lottie-1" />
-                <div>RETURNS</div>
-                <section />
-              </div>
-            )
-          })}
-        </animated.div>
-      </div>
-      <div className="lines line-2">
-        <animated.div
-          className="lines-inner"
-          style={{
-            transform: to([textX], (x) => `translate3d(-${50 - x}%, 0, 0)`),
-          }}
-        >
-          {Array.from(new Array(2), (_, i) => i).map((o) => {
-            return (
-              <div key={o}>
-                <div>HAVE A</div>
-                <div className="lotties lottie-2" />
-                <div>JOYFUL DAY</div>
-                <section />
-              </div>
-            )
-          })}
-        </animated.div>
-      </div>
-      <div className="lines line-3">
-        <animated.div
-          className="lines-inner"
-          style={{
-            transform: to([textX], (x) => `translate3d(-${x}%, 0, 0)`),
-          }}
-        >
-          {Array.from(new Array(2), (_, i) => i).map((o) => {
-            return (
-              <div key={o}>
-                <div>MY FRIEND</div>
-                <img src="https://wxa.wxs.qq.com/wxad-design/cpy/heart.svg" />
-                <div>CPY</div>
-                <section />
-              </div>
-            )
-          })}
-        </animated.div>
-      </div>
-      <div className="carousel">
-        <div className="carousel-bg">
-          <div>
-            <span className="carousel-letter">c</span>
-            <span className="carousel-letter">i</span>
-            <span className="carousel-letter">c</span>
-            <span className="carousel-letter">i</span>
-            <span className="carousel-letter">n</span>
-            <span className="carousel-letter">n</span>
-            <span className="carousel-letter">u</span>
-            <span className="carousel-letter">s</span>
-          </div>
-
-          <div>
-            <span className="carousel-text">c</span>
-            <span className="carousel-text">i</span>
-            <span className="carousel-text">c</span>
-            <span className="carousel-text">i</span>
-            <span className="carousel-text">n</span>
-            <span className="carousel-text">n</span>
-            <span className="carousel-text">u</span>
-            <span className="carousel-text">s</span>
-          </div>
-        </div>
-        <div className="carousel-inner-wrapper" {...bindDrag()}>
+        <div className="lines line-1">
           <animated.div
-            className="carousel-inner"
+            className="lines-inner"
             style={{
-              transform: to([carouselX], (x) => `translate3d(${x}px, 0, 0)`),
+              transform: to([textX], (x) => `translate3d(-${x}%, 0, 0)`),
             }}
           >
-            {carouselImages.map((o, i) => (
-              <animated.div
-                key={o}
-                className={`carousel-item carousel-item-${i + 1}`}
-                style={{
-                  transformOrigin: to(
-                    [carouselX],
-                    (x) => `${(-(x + 280 * (i - 1)) / 280) * 50}% 100%`
-                  ),
-                  transform: to(
-                    [carouselX],
-                    (x) =>
-                      `translate3d(${
-                        8 + ((x + 280 * (i - 1)) / 280) * 8
-                      }%, 0, 0) rotate(${((x + 280 * i - 1) / 280) * 10}deg)`
-                  ),
-                }}
-              >
-                <img src={o} />
-              </animated.div>
-            ))}
+            {Array.from(new Array(2), (_, i) => i).map((o) => {
+              return (
+                <div key={o}>
+                  <div>MANY HAPPY</div>
+                  <div className="lotties lottie-1" />
+                  <div>RETURNS</div>
+                  <section />
+                </div>
+              )
+            })}
           </animated.div>
         </div>
-        {/* <div className="carousel-arrow carousel-arrow-left" /> */}
-        {/* <div className="carousel-arrow carousel-arrow-right" /> */}
-      </div>
-      <div className="footer">
-        <div>
-          <span className="footer-link">cicinn.us</span> is a tiny page made for
-          celebrating the birthday of <span className="footer-aragakey">Cicinnus Cheng</span> who's an individual
-          possesses both inner and outer beauty, and most importantly a
-          supportive teammate and friend of mine.
+        <div className="lines line-2">
+          <animated.div
+            className="lines-inner"
+            style={{
+              transform: to([textX], (x) => `translate3d(-${50 - x}%, 0, 0)`),
+            }}
+          >
+            {Array.from(new Array(2), (_, i) => i).map((o) => {
+              return (
+                <div key={o}>
+                  <div>HAVE A</div>
+                  <div className="lotties lottie-2" />
+                  <div>JOYFUL DAY</div>
+                  <section />
+                </div>
+              )
+            })}
+          </animated.div>
         </div>
-        <div>
-          <span className="footer-link">cicinn.us</span> is designed &
-          handcrafted by <span className="footer-aragakey">Aragakey</span> who
-          paid ¥53.95 for this domain name. It'll expire on January 31, 2024. If
-          you need to renew it, you can contact me then.
+        <div className="lines line-3">
+          <animated.div
+            className="lines-inner"
+            style={{
+              transform: to([textX], (x) => `translate3d(-${x}%, 0, 0)`),
+            }}
+          >
+            {Array.from(new Array(2), (_, i) => i).map((o) => {
+              return (
+                <div key={o}>
+                  <div>MY FRIEND</div>
+                  <img src="https://wxa.wxs.qq.com/wxad-design/cpy/heart.svg" />
+                  <div>CPY</div>
+                  <section />
+                </div>
+              )
+            })}
+          </animated.div>
         </div>
-        <div>
-          <span className="footer-link">cicinn.us</span> may load relatively
-          slowly, cuz it's hosted on github which is free. If you want it to be
-          faster, you'll have to figure it out yourself (or pay me).
-        </div>
-        <div>
-          Or one day you want to turn it into your own personal site, you can
-          pay me as well.
-        </div>
-        <div>Anyway, happy birthday to you <span className="footer-aragakey">Cicinnus</span>. Hope you like it (if you don't, I'll never talk to you for the rest of my life).</div>
+        <div className="carousel">
+          <div className="carousel-bg">
+            <div>
+              <span className="carousel-letter">c</span>
+              <span className="carousel-letter">i</span>
+              <span className="carousel-letter">c</span>
+              <span className="carousel-letter">i</span>
+              <span className="carousel-letter">n</span>
+              <span className="carousel-letter">n</span>
+              <span className="carousel-letter">u</span>
+              <span className="carousel-letter">s</span>
+            </div>
 
-        <section className="cake">
-          <div className="velas">
-            <div className="fuego"></div>
-            <div className="fuego"></div>
-            <div className="fuego"></div>
-            <div className="fuego"></div>
-            <div className="fuego"></div>
+            <div>
+              <span className="carousel-text">c</span>
+              <span className="carousel-text">i</span>
+              <span className="carousel-text">c</span>
+              <span className="carousel-text">i</span>
+              <span className="carousel-text">n</span>
+              <span className="carousel-text">n</span>
+              <span className="carousel-text">u</span>
+              <span className="carousel-text">s</span>
+            </div>
           </div>
-          <div className="cobertura"></div>
-          <div className="bizcocho"></div>
-        </section>
+          <div className="carousel-inner-wrapper" {...bindDrag()}>
+            <animated.div
+              className="carousel-inner"
+              style={{
+                transform: to([carouselX], (x) => `translate3d(${x}px, 0, 0)`),
+              }}
+            >
+              {carouselImages.map((o, i) => (
+                <animated.div
+                  key={o}
+                  className={`carousel-item carousel-item-${i + 1}`}
+                  style={{
+                    transformOrigin: to(
+                      [carouselX],
+                      (x) => `${(-(x + 280 * (i - 1)) / 280) * 50}% 100%`
+                    ),
+                    transform: to(
+                      [carouselX],
+                      (x) =>
+                        `translate3d(${
+                          8 + ((x + 280 * (i - 1)) / 280) * 8
+                        }%, 0, 0) rotate(${((x + 280 * i - 1) / 280) * 10}deg)`
+                    ),
+                  }}
+                >
+                  <img src={o} />
+                </animated.div>
+              ))}
+            </animated.div>
+          </div>
+          {/* <div className="carousel-arrow carousel-arrow-left" /> */}
+          {/* <div className="carousel-arrow carousel-arrow-right" /> */}
+        </div>
+        <div className="footer">
+          <div>
+            <span className="footer-link">cicinn.us</span> is a tiny page made
+            for celebrating the birthday of{" "}
+            <span className="footer-aragakey">Cicinnus Cheng</span> who's an
+            individual possesses both inner and outer beauty, and most
+            importantly a supportive teammate and friend of mine.
+          </div>
+          <div>
+            <span className="footer-link">cicinn.us</span> is designed &
+            handcrafted by <span className="footer-aragakey">Aragakey</span> who
+            paid ¥53.95 for this domain name. It'll expire on January 31, 2024.
+            If you need to renew it, you can contact me then.
+          </div>
+          <div>
+            <span className="footer-link">cicinn.us</span> may load relatively
+            slowly, cuz it's hosted on github which is free. If you want it to
+            be faster, you'll have to figure it out yourself (or pay me).
+          </div>
+          <div>
+            Or one day you want to turn it into your own personal site, you can
+            pay me as well.
+          </div>
+          <div>
+            Anyway, happy birthday to you{" "}
+            <span className="footer-aragakey">Cicinnus</span>. Hope you like it
+            (if you don't, I'll never talk to you for the rest of my life).
+          </div>
 
-        <footer>
-          <span data-role="footer-letters">c</span>
-          <span data-role="footer-letters">i</span>
-          <span data-role="footer-letters">c</span>
-          <span data-role="footer-letters">i</span>
-          <span data-role="footer-letters">n</span>
-          <span data-role="footer-letters">n</span>
-          <span data-role="footer-letters">.</span>
-          <span data-role="footer-letters">u</span>
-          <span data-role="footer-letters">s</span>
-        </footer>
+          <section className="cake">
+            <div className="velas">
+              <div className="fuego"></div>
+              <div className="fuego"></div>
+              <div className="fuego"></div>
+              <div className="fuego"></div>
+              <div className="fuego"></div>
+            </div>
+            <div className="cobertura"></div>
+            <div className="bizcocho"></div>
+          </section>
+
+          <footer>
+            <span data-role="footer-letters">c</span>
+            <span data-role="footer-letters">i</span>
+            <span data-role="footer-letters">c</span>
+            <span data-role="footer-letters">i</span>
+            <span data-role="footer-letters">n</span>
+            <span data-role="footer-letters">n</span>
+            <span data-role="footer-letters">.</span>
+            <span data-role="footer-letters">u</span>
+            <span data-role="footer-letters">s</span>
+          </footer>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
